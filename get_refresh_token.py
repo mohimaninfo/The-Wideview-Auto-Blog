b3cbc2976c0e50c73b8820a578b650c0a640aa36
M. Mohiuddin <mohimaninfo@gmail.com>
2026-05-15 23:31:41 +0600

    Refactor token retrieval process in get_refresh_token.py
    Updated the script to manually generate the authorization URL and fetch tokens after user input.

--- get_refresh_token.py
+++ get_refresh_token.py
@@ -1,4 +1,5 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
+import json
 
 SCOPES = ["https://www.googleapis.com/auth/blogger"]
 
@@ -7,7 +8,26 @@ flow = InstalledAppFlow.from_client_secrets_file(
     SCOPES
 )
 
-creds = flow.run_local_server(port=0)
+# Generate the auth URL manually
+auth_url, _ = flow.authorization_url(
+    access_type="offline",
+    prompt="consent"
+)
 
-print("\nREFRESH TOKEN:\n", creds.refresh_token)
-print("\nACCESS TOKEN:\n", creds.token)
+print("\nOpen this URL in your browser:\n")
+print(auth_url)
+print()
+
+code = input("Paste the authorization code here: ")
+
+flow.fetch_token(code=code)
+creds = flow.credentials
+
+print("\n=== COPY THIS INTO YOUR GITHUB SECRET ===\n")
+print(json.dumps({
+    "token": creds.token,
+    "refresh_token": creds.refresh_token,
+    "token_uri": creds.token_uri,
+    "client_id": creds.client_id,
+    "client_secret": creds.client_secret
+}, indent=2))
